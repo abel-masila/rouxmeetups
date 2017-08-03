0bdc823337a4207fc2f5d742fb3ac7f4efb403af
abel-masila <abelmasila@gmail.com>
2017-08-03 12:36:13 +0300

    Finished App

--- routes/index.js
+++ routes/index.js
@@ -5,36 +5,50 @@ var appdata = require('../data.json');
 /* GET home page. */
 router.get('/', function(req, res) {
   var myArtwork = [];
+  var myArtists=[];
+  myArtists=appdata.speakers;
   appdata.speakers.forEach(function(item) {
     myArtwork = myArtwork.concat(item.artwork);
   });
   res.render('index', {
     title: 'Home',
-    artwork: myArtwork
+    artwork: myArtwork,
+    artists:myArtists,
+    page:'home'
   });
 });
 /* GET Speakers page. */
 router.get('/speakers', function(req, res) {
   var myArtwork = [];
+  var myArtists=[];
+   
+  myArtists=appdata.speakers;
   appdata.speakers.forEach(function(item) {
     myArtwork = myArtwork.concat(item.artwork);
   });
   res.render('speakers', {
     title: 'Speakers',
-    artwork: myArtwork
+    artwork: myArtwork,
+     artists:myArtists,
+     page:'artistList'
   });
 });
-/* GET Speakers page. */
+/* GET Speakers Detail page. */
 router.get('/speakers/:speakerid', function(req, res) {
   var myArtwork = [];
+  var myArtists=[];
+
   appdata.speakers.forEach(function(item) {
     if(item.shortname==req.params.speakerid){
+      myArtists.push(item);
        myArtwork = myArtwork.concat(item.artwork);
     }
   });
   res.render('speakers', {
     title: 'Speakers',
-    artwork: myArtwork
+    artwork: myArtwork,
+    artists:myArtists,
+    page:'artistDetail'
   });
 });
 
